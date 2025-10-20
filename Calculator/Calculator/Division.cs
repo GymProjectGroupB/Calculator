9999201a979355b9a267a0acf7aaab195866f4f1
venujan nagendirakumar <venujan25@outlook.com>
2025-10-20 12:37:44 +0200

    Division

---
+++ Calculator/Calculator/Division.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Division
+    {
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Nämnaren (b) får inte vara 0.");
+            }
+
+            return a / b;
+        }
+    }
+}
+
+// venu
